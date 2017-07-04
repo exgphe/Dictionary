f93cb8089f15d29fa0abb850ea28e6c576557357
exgphe <exgphe@icloud.com>
2017-07-04 18:02:09 +0800

    Initial Commit

---
+++ Dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dictionary
+//
+//  Created by Xiaolin Wang on 04/07/2017.
+//  Copyright © 2017 Pride1952. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
